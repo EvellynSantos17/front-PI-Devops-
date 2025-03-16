ec3f1525c015fe865e412a4e1b5ff10d5ae55222
EvellynSantos17 <evellynaparecida0@gmail.com>
2025-03-15 22:33:50 -0300

    fix:Finalizado

--- src/components/layout/base-reader.jsx
+++ src/components/layout/base-reader.jsx
@@ -1,8 +1,13 @@
 import { getDisplayName } from "next/dist/shared/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+/*import Image from "next/image";*/
+
 
 export default function BaseHeader(){
+    const userAuth = false
+    const userName = "Eve Linda"
+
     return(
         <header className="flex items-center justify-between p-4 bg-white shadow-md">
             <Image 
@@ -12,9 +17,9 @@ export default function BaseHeader(){
               width={50}
                height={60}
             /> 
-            <div className="flex items-center gap-2 bg-gray-100 rounded-2xl px-3 py-2 w-[400px]" >
+            <div className="flex items-center gap-2 bg-gray-100 rounded-full px-4 py-2 w-fix " >
                 <input 
-                    className="bg-transparent border-none outline-none flex-1"
+                    className="bg-transparent border-none outline-none flex-1 w-[300px]"
                     type="text" 
                     placeholder="Digite Aqui..."
                 /> 
@@ -56,21 +61,43 @@ export default function BaseHeader(){
                     </Link>
                 </li>
                 <li>
-                    <Link className="hover:bg-slate-600 px-2 py-1 rounded-2xl hover:text-white "
-                    href={"/entrar"}
-                    >
-                        ENTRAR
-                    </Link>
-                </li>
-                <li>
-                    <Link className="hover:bg-laranjaProdunfo px-2 py-1 rounded-2xl hover:text-white"
-                    href={"/cadastre-se"}
-                    > 
-                        CADASTRE-SE
-                    </Link>
-                </li>
+                {userAuth ? ( // Se o usuário estiver autenticado, exibe a imagem e o nome
+    <div className="p-2 rounded-lg bg-transparent  flex items-center gap-2">
+        <h1 className="text-lg underline">{userName}</h1> 
+        <Image
+            className="rounded-full" 
+            alt="img" 
+            src="/eve.png"
+            width={35} 
+            height={35}
+        />
+    </div>
+) : ( // Se NÃO estiver autenticado, exibe os botões de entrar e cadastro
+    <div className="flex gap-2"> 
+        <Link 
+            className="hover:bg-slate-600 px-2 py-1 rounded-2xl hover:text-white" 
+            href={"/entrar"}
+        >
+            ENTRAR
+        </Link>
+        <Link 
+            className="hover:bg-laranjaProdunfo px-2 py-1 rounded-2xl hover:text-white"
+            href={"/cadastre-se"}
+        > 
+            CADASTRE-SE
+        </Link>
+    </div>
+)}
+
+ 
+</li>
+
+                
             </ul>
         </header>
 
     )
 }
+
+
+
