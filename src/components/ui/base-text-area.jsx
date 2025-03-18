fa7f1410594e85a95b43d09bc3df2ed2277f7b83
aristotelesAlves <arystotelys@gmail.com>
2025-03-18 16:44:31 -0300

    :wrench: chore: Atualização e correção do segundo formulario de cadastro - Organizando em components e separando client componets

--- src/components/ui/base-text-area.jsx
+++ src/components/ui/base-text-area.jsx
@@ -1,10 +1,9 @@
 "use client";
 
-export function InputField({ label, required, inputStyle = "login", name, error, type = "text", value, onChange, onBlur, placeholder }) {
+export function BaseTextArea({ label, inputStyle = "form", name, error, type = "text", value, onChange, onBlur, placeholder }) {
 
     const style = {
-        login: 'border border-b-4 p-2 rounded-3xl w-full border-[#D9D9D9] shadow-md mt-1',
-        form: 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm'
+        form: 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm h-24 resize-none'
     }
         
     return (
@@ -14,10 +13,9 @@ export function InputField({ label, required, inputStyle = "login", name, error,
                     {label}
                 </label>
             )}
-            <input 
+            <textarea 
                 className={style[inputStyle]} 
                 placeholder={placeholder}
-                required={required}
                 name={name} 
                 id={name} 
                 type={type}
